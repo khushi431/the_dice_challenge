e34808ce34fcbab4f3d7cabb51220b61ab73d75b
Delta student <arorakhushi431@gmail.com>
2024-01-12 19:48:03 +0530

    add

--- app.js
+++ app.js
@@ -14,10 +14,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 //if player 1 wins
 if(randomNumber1> randomNumber2){
-    document.querySelector("h1").innerHTML ="Palyer 1 Wins!"
+    document.querySelector("h1").innerHTML ="Player 1 Wins!"
 }
 else if(randomNumber2> randomNumber1){
-    document.querySelector("h1").innerHTML ="Palyer 2 Wins!"
+    document.querySelector("h1").innerHTML ="Player 2 Wins!"
 }
 else {
     document.querySelector("h1").innerHTML ="Draw!"
